8c5d61dd1139cdebd0de4838260ffee840c2df44
UtkarshKr007 <kumar375@morris.umn.edu>
2019-10-08 11:15:24 -0500

    finish the bash script verison of summarizing directories. all test on 'summarize_tree.sh'pass.

---
+++ summarize_tree/summarize_tree.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+inputPath=$1
+
+echo "There were" "$(find "$inputPath" -type d  | wc -l)" "directories."
+echo "There were" "$(find "$inputPath" -type f  | wc -l)" "regular files."
+
